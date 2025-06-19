0a3ea2af6384b59ce0f513e34a3c8f62678e6297
FuncZero <contato@funczero.xyz>
2025-06-19 19:18:23 -0300

    Atualização automática

--- FuncZone/src/logger/logger.ts
+++ FuncZone/src/logger/logger.ts
@@ -1,1 +1,15 @@
+import { createLogger, format, transports } from 'winston';
 
+export const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp({ format: 'HH:mm:ss' }),
+    format.colorize(),
+    format.printf(({ level, message, timestamp }) => `[${timestamp}] ${level}: ${message}`)
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
