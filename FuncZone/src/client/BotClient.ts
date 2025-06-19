274fe5b50ca22b8c6c45994623eb184503e75b60
FuncZero <contato@funczero.xyz>
2025-06-19 19:11:04 -0300

    Atualização automática

--- FuncZone/src/client/BotClient.ts
+++ FuncZone/src/client/BotClient.ts
@@ -1,1 +1,17 @@
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import type { Command } from '../types/Bot';
 
+export class BotClient extends Client {
+  public commands = new Collection<string, Command>();
+
+  constructor() {
+    super({
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+      ],
+    });
+  }
+}
