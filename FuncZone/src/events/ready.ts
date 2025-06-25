c7c6cbea868b29945a598c5d819627bc632da898
FuncZero <contato@funczero.xyz>
2025-06-25 20:39:38 -0300

    Atualização automática

---
+++ FuncZone/src/events/ready.ts
@@ -0,0 +1,8 @@
+import { Client } from 'discord.js';
+import { presenceConfig } from '../config/presence';
+import { logger } from '../utils/logger';
+
+export default async function onReady(client: Client): Promise<void> {
+  logger.info(`Bot online como ${client.user?.tag}`);
+  client.user?.setPresence(presenceConfig);
+}
