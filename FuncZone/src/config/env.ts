8a3ae572af2e25505128ee2b87ba5ccdfe9dccae
FuncZero <contato@funczero.xyz>
2025-06-25 20:06:41 -0300

    Delete FuncZone directory

--- FuncZone/src/config/env.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-
-export const config = {
-  TOKEN: process.env.TOKEN as string,
-  PREFIX: process.env.PREFIX || '.',
-};
--- FuncZone/src/events/ready.ts
+++
@@ -1,5 +0,0 @@
-import { BotClient } from '../client/BotClient';
-
-export default (client: BotClient) => {
-  console.log(`[FuncZone] Logado como ${client.user?.tag}`);
-};
