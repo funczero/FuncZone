d6fc8c4cce39b0a6b77214fb6cbd216bc1870a92
FuncZero <contato@funczero.xyz>
2025-06-25 20:11:22 -0300

    Atualização automática

---
+++ FuncZone/src/utils/logger.ts
@@ -0,0 +1,26 @@
+import winston from 'winston';
+import chalk from 'chalk';
+
+const customFormat = winston.format.printf(({ level, message, timestamp }) => {
+  const color = {
+    info: chalk.blueBright,
+    warn: chalk.yellow,
+    error: chalk.red,
+    debug: chalk.gray,
+  }[level] || chalk.white;
+
+  return `${chalk.dim(timestamp)} [${color(level.toUpperCase())}] ${message}`;
+});
+
+export const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'HH:mm:ss' }),
+    customFormat
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' })
+  ],
+});
