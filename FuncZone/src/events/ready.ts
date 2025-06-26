e0e6c60e64db9e773aaf64ab3a8f99ff35870b08
FuncZero <contato@funczero.xyz>
2025-06-25 23:16:56 -0300

    fix

--- FuncZone/src/events/ready.ts
+++ FuncZone/src/events/ready.ts
@@ -1,8 +1,7 @@
 import { Client } from 'discord.js';
 import { presenceConfig } from '../config/presence';
 import { logger } from '../utils/logger';
-//!
+
 export default async function onReady(client: Client): Promise<void> {
-  logger.info(`🥺🥺 ${client.user?.tag}`);
   client.user?.setPresence(presenceConfig);
 }
