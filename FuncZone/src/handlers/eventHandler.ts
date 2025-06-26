f79efcff8b349495ed893c5e2f25034e05b68077
FuncZero <contato@funczero.xyz>
2025-06-26 06:59:06 -0300

    Atualização automática

--- FuncZone/src/handlers/eventHandler.ts
+++ FuncZone/src/handlers/eventHandler.ts
@@ -1,8 +1,8 @@
-import { Client } from 'discord.js';
 import { readdirSync } from 'fs';
 import path from 'path';
+import { BotClient } from '../client/BotClient';
 
-export async function loadEvents(client: Client) {
+export async function loadEvents(client: BotClient) {
   const eventsPath = path.join(__dirname, '..', 'events');
   const eventFiles = readdirSync(eventsPath).filter(file => file.endsWith('.ts') || file.endsWith('.js'));
 
