216ca57f22e7da2661c18dee024c62a8866d738c
FuncZero <contato@funczero.xyz>
2025-06-26 21:57:40 -0300

    refactor(BotClient): aplica aliases nas importações para padronizar estrutura modular

--- FuncZone/src/client/BotClient.ts
+++ FuncZone/src/client/BotClient.ts
@@ -4,8 +4,8 @@ import {
   Partials,
   Collection,
 } from 'discord.js';
-import { logger } from '@utils/logger';
-import type { Command } from '@types/index';
+import { logger } from '@utils';
+import type { Command } from '@types';
 
 /**
  * Cliente estendido do Discord com estrutura personalizada para o bot.
