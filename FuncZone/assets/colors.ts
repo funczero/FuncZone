ebfa257b0042a5ecb48c941717834d7d78d7dae8
FuncZero <contato@funczero.xyz>
2025-06-29 12:36:28 -0300

    Delete FuncZone directory

--- FuncZone/assets/colors.ts
+++
@@ -1,6 +0,0 @@
-export const colors = {
-  green: '#2ECC71',
-  yellow: '#F3CF3F',
-  red: '#FE3838',
-  blue: '#8C9CFC',
-};
--- FuncZone/src/events/ready.ts
+++
@@ -1,7 +0,0 @@
-import { Client } from 'discord.js';
-import { presenceConfig } from '../config/presence';
-import { logger } from '../utils/logger';
-
-export default async function onReady(client: Client): Promise<void> {
-  client.user?.setPresence(presenceConfig);
-}
