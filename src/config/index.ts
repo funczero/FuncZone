c978cc1cd97f61da7a8d276dcf202744c1b7841e
FuncZero <contato@funczero.xyz>
2025-06-29 12:43:07 -0300

    feat(config): adicionar objeto env para centralizar variáveis de ambiente

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+export const env = {
+  discordToken: process.env.DISCORD_TOKEN!,
+  welcomeChannelId: process.env.WELCOME_CHANNEL_ID!,
+  boostChannelId: process.env.BOOST_CHANNEL_ID!,
+  rulesUrl: process.env.RULES_URL!,
+  announcementsUrl: process.env.ANNOUNCEMENTS_URL!,
+  supportUrl: process.env.SUPPORT_URL!,
+};
