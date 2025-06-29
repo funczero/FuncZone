a6dda52d1c9a581d111b8b286a291e5c4429e8e7
FuncZero <contato@funczero.xyz>
2025-06-28 21:25:42 -0300

    Update icons.ts

--- FuncZone/assets/icons.ts
+++ FuncZone/assets/icons.ts
@@ -2,4 +2,5 @@ export const icons = {
   success: '<:success:1387940358348083343>',
   error: '<:error:1387940402891587634>',
   warning: '<:warning:1387941716086231130>',
+  attention: 'https://bit.ly/3Ypxwnl'
 };
